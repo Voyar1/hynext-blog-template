30d507a8d06d047398af942a795c7c0f1f580629
Arkadiusz <areq12x@gmail.com>
2023-02-20 20:44:15 +0100

    Added responsive nav

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -2,13 +2,19 @@ import Link from "next/link";
 import React, { useState, useEffect } from "react";
 import styles from "./header.module.css";
 import { getCategories } from "@/services";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Header = () => {
   const [categories, setCategories] = useState([]);
+  const [navOpen, setNavOpen] = useState(false);
 
   useEffect(() => {
     getCategories().then((newCategories) => setCategories(newCategories));
   }, []);
+
+  const toggleNavHandler = () => {
+    setNavOpen(!navOpen);
+  };
 
   return (
     <header className={styles.header}>
@@ -18,8 +24,13 @@ const Header = () => {
             HYNEXT
           </Link>
         </div>
-        <nav className={styles.navigation}>
+        <nav
+          className={`${styles.navigation} ${navOpen ? styles.navActive : ""}`}
+        >
           <ul className={styles.navigation__list}>
+            <button className={styles.closeBtn} onClick={toggleNavHandler}>
+              <FaTimes />
+            </button>
             {categories.map((cat) => {
               return (
                 <li key={cat.slug} className={styles.navigation__item}>
@@ -34,6 +45,14 @@ const Header = () => {
             })}
           </ul>
         </nav>
+        <button
+          className={`${styles.hamburger} ${
+            navOpen ? styles.hamburgerNone : ""
+          }`}
+          onClick={toggleNavHandler}
+        >
+          <FaBars />
+        </button>
       </div>
     </header>
   );
