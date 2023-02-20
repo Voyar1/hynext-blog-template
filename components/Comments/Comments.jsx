0432f21a1bd68b970aa17b75bb85f93ac7aaa62d
Arkadiusz <areq12x@gmail.com>
2023-02-20 20:58:41 +0100

    improved comments order in posts

--- components/Comments/Comments.jsx
+++ components/Comments/Comments.jsx
@@ -19,18 +19,20 @@ const Comments = ({ slug }) => {
       <span className={styles.commentsSectionTitle}>Comments</span>
       <div className={styles.comments}>
         <span className={styles.commentsValue}>{comments.length} Comments</span>
-        {comments.map((comment) => {
-          return (
-            <div className={styles.comment} key={comment.createdAt}>
-              <p className={styles.commentInfo}>
-                <span className={styles.commentAuthor}>{comment.name}</span> on{" "}
-                {moment(comment.date).format("MMM Do YYYY")}
-              </p>
-              <p>{comment.comment}</p>
-              <p></p>
-            </div>
-          );
-        })}
+        {comments
+          .map((comment) => {
+            return (
+              <div className={styles.comment} key={comment.createdAt}>
+                <p className={styles.commentInfo}>
+                  <span className={styles.commentAuthor}>{comment.name}</span>{" "}
+                  on {moment(comment.createdAt).format("MMM Do YYYY")}
+                </p>
+                <p>{comment.comment}</p>
+                <p></p>
+              </div>
+            );
+          })
+          .reverse()}
       </div>
     </div>
   );
